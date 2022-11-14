0373259d2c8d9068ce326460e36747bf4aaccfef
rszaknyeri <RolandTibor.Szaknyeri@goto.com>
2022-11-14 13:01:03 +0100

    initial

---
+++ Molnar/Matrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molnar
+{
+    internal class Matrix
+    {
+        internal int Width { get; init; }
+
+        internal int Height { get; init; }
+
+        internal int R { get; private set; }
+
+        internal string[,] NdArray { get; set; }
+
+        public Matrix(int width, int height, int r, string[] matrixValues)
+        {
+            Width = width;
+            Height = height;
+            R = r;
+
+            NdArray = new string[height, width];
+
+            FillMatrix(matrixValues);
+
+        }
+
+        public void SetR(int value)
+        {
+            this.R = value;
+        }
+
+        public void DrawMatrix()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write(NdArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void RotateMatrix()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        private void FillMatrix(string[] matrixValues)
+        {
+            for (int i = 0; i < matrixValues.Count(); i++)
+            {
+                var row = matrixValues[0].Split();
+
+                for (int j = 0; j < row.Length; j++)
+                {
+                    NdArray[i, j] = row[j];
+                }
+            }
+        }
+    }
+}
---
+++ Molnar/Program.cs
@@ -0,0 +1,98 @@
+﻿using Molnar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var matrix = GetParams();
+        matrix.DrawMatrix();
+    }
+
+    static Matrix GetParams()
+    {
+        bool result = false;
+
+        int widht = 0;
+        int height = 0;
+        int rotation = 0;
+        string[] values = new string[100];
+        
+        do
+        {
+            try
+            {
+                Console.WriteLine("please provide matrix dimenion, format: W H");
+                var dimensionValueIn = Console.ReadLine();
+                var dim = GetMatrixDimensions(dimensionValueIn!);
+                widht = dim.Item1;
+                height = dim.Item2;
+
+
+                Console.WriteLine("please provide rotation count");
+                var rotationValueIn = Console.ReadLine();
+                rotation = GetRotationValue(rotationValueIn!);
+
+                Console.WriteLine("please provide matrix values");
+                var vals = GetMatrixValues(dim.Item2);
+                values = vals;
+
+                result = true;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"invalid param {e.Message}");
+            }
+        } while (!result);
+
+        return new Matrix(widht, height, rotation, values);
+    }
+
+    static Tuple<int, int> GetMatrixDimensions(string values)
+    {
+        var parsedValues = values.Split(' ');
+
+        if (parsedValues.Length != 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(values));
+        }
+
+        bool widthResult = int.TryParse(parsedValues[0], out int mWidth);
+
+        bool heightResult = int.TryParse(parsedValues[1], out int mHeight);
+
+        if ((widthResult && heightResult) &&
+            (0 < mWidth && mWidth <= 100) &&
+            (0 < mHeight && mHeight <= 100)
+            )
+        {
+            var dimensions = new Tuple<int, int>(mWidth, mHeight);
+            return dimensions;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(values));
+    }
+
+    static int GetRotationValue(string rotation)
+    {
+        if (int.TryParse(rotation, out int rCount))
+        {
+            return rCount;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(rotation));
+    }
+
+    static string[] GetMatrixValues(int height)
+    {
+        string[] array = new string[height];
+        int counter = 0;
+
+        do
+        {
+            array[counter] = Console.ReadLine()!;
+            counter++;
+        } while (counter < height);
+
+        return array;
+    }
+}
